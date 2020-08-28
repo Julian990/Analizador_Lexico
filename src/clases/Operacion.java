54aeca8eb6e79e5b1de07209f898a3283a31b909
julian990 <malpud2015@gmail.com>
2020-08-28 14:21:38 -0500

    first commit

---
+++ src/clases/Operacion.java
@@ -0,0 +1,36 @@
+
+package clases;
+
+
+public class Operacion {
+    
+    int x,y;
+
+    public Operacion(int a, int b){
+
+        x=a;
+        y=b;
+
+    }
+    public int getResta(){
+        return x-y;
+    }
+   
+    public int getSuma(){
+        return x+y;
+    }
+    
+    public int getMultiplicacion(){
+        return x*y;
+    }   
+    public int getDivision(){
+        return x/y;
+    }
+    public int getPotencia(){
+        for(int i=1; i<= y;i++){
+            x=x*x;
+        }
+        return x;
+
+    }
+}
